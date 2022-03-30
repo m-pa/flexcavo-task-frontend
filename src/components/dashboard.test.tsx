57d7835bf2e72b8af164556638ce8110a2f56827
m-pa <onen7701@gmail.com>
2022-03-30 16:45:16 +0200

    use header and details component

--- src/components/dashboard.test.tsx
+++ src/components/dashboard.test.tsx
@@ -2,8 +2,13 @@ import 'regenerator-runtime/runtime'
 import React from 'react'
 import { cleanup, render } from '@testing-library/react'
 import { Dashboard } from './dashboard'
+import { StateProvider } from '../lib/stateProvider'
 
 afterEach(cleanup)
+
+const withProvider = (Dashboard: any) => (props: any) => (
+  <StateProvider><Dashboard /></StateProvider>
+)
 
 describe('Dashboard', () => {
   it('exports a component', () => {
@@ -11,9 +16,17 @@ describe('Dashboard', () => {
     expect(Dashboard).toBeInstanceOf(Function)
   })
 
-  it('has a container', async () => {
-    const container = render(<Dashboard />)
+  it('has a header', async () => {
+    const DashboardWithProvider = withProvider(Dashboard)
+    const container = render(<DashboardWithProvider />)
     const heading = await container.findByText('Dashboard')
+    expect(heading).toBeTruthy()
+  })
+
+  it('has equipment details', async () => {
+    const DashboardWithProvider = withProvider(Dashboard)
+    const container = render(<DashboardWithProvider />)
+    const heading = await container.findByText('Equipment Details')
     expect(heading).toBeTruthy()
   })
 })
--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,23 +1,57 @@
-import React from 'react'
-import { Container, makeStyles } from '@material-ui/core'
-import { LineH4 } from '@flexcavo/ui-kit'
+import React, { useContext } from 'react'
+import { Container, Grid, makeStyles } from '@material-ui/core'
+import { ListItem, Icon, theme } from '@flexcavo/ui-kit'
+import { stateContext } from '../lib/stateProvider'
+import { DashboardHeader } from './dashboard-header'
+import { DashboardEquipmentDetails } from './dashboard-equipment-details'
 
 const useStyles = makeStyles({
   dashboard: {
     marginTop: '300px',
-    padding: '20px',
+    padding: '100px',
     background: '#fff',
-    borderRadius: '1px',
-    border: '1px solid #eee'
+    borderRadius: theme.shape.borderRadius,
+    border: '2px solid #eee',
+    paddingBottom: '200px'
+  },
+  item: {
+    background: 'fuchsia',
+    minWidth: '300px'
+  },
+  container: {
+    marginTop: '70px'
   }
 })
 
 export const Dashboard = (): JSX.Element => {
-  const { dashboard } = useStyles()
+  const { dashboard, item, container } = useStyles()
+  const state = useContext(stateContext)
 
-  return (
-    <Container className={dashboard}>
-      <LineH4 label='Dashboard' />
-    </Container>
-  )
+  return state === null
+    ? (
+      <Container className={dashboard}>
+        loading...
+      </Container>
+      )
+    : (
+      <Container className={dashboard}>
+        <DashboardHeader snapshotTime={state?.equipmentHeader.data.snapshotTime} />
+        <DashboardEquipmentDetails equipmentHeader={state.equipmentHeader.data} />
+        <Grid container className={container} spacing={3} justifyContent='space-between'>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeIdleHours.data.hour} secondaryText='Cumulative Hours Idle' icon={<Icon name='HourGlassEmpty' />} /></Grid>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeOperatingHours.data.hour} secondaryText='Cumulative Hours Operating' icon={<Icon name='AccessTime' />} /></Grid>
+        </Grid>
+        <Grid container className={container} spacing={3} justifyContent='space-between'>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeIdleHours.data.hour} /></Grid>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeIdleHours.data.hour} /></Grid>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeIdleHours.data.hour} /></Grid>
+        </Grid>
+        <Grid container className={container} spacing={3} justifyContent='space-between'>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeIdleHours.data.hour} /></Grid>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeIdleHours.data.hour} /></Grid>
+          <Grid className={item} item><ListItem primaryText={state?.cumulativeIdleHours.data.hour} /></Grid>
+        </Grid>
+
+      </Container>
+      )
 }
